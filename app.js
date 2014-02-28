911b3cd85f8a695b0870ab5c86a3315beee1176b
Craig Pemberton <craig.pemberton@blackbaud.com>
2014-02-27 18:31:33 -0600

    Trying to get all the vote results at once

--- app.js
+++ app.js
@@ -16,47 +16,31 @@ var sfCredentials = {
 var totalVotes;
 var followingVotes;
 var followingVotesName = 'Susan G. Komen';
-var followingTable = 'Survey_Response__c'
-var followingField = 'Vote__c'
+var followingTable = 'Survey_Response__c';
+var followingField = 'Vote__c';
 
-function authenticationCallback(error, response){
-	console.log('In authenticaion callback.');
-	genericCallback(error, response);
-	console.log('Cached Token: ' + connection.oauth.access_token);
-	main();
+function print(object){
+	return JSON.stringify(object);
 }
 
-function queryCallback(error, response){
-	console.log('In query callback.');
-	genericCallback(error, response);
-}
-
-function genericCallback(error, response){
+function handleCallbackErrors(error, response){
 	if(error){
 		console.log('Error: ' + error);
 	}
 	else{
-		console.log('Got response ' + response);
-		//if(response && response.records){
-			//print(response.records);
-		//}
+		//console.log('Got response ' + print(response));
 	}
 }
 
-function print(object){
-	console.log(JSON.stringify(object));
-}
-
-function insertCallback(error, response){
-	console.log('In insertCallback');
-	genericCallback(error, response);
-}
-
-function initializingTotals(tableName,callbackFunction){
-	connection.query({query:'SELECT Id FROM '+tableName}, function(error,response){		
-		totalVotes = response.records.length;
-		console.log("Votes:"+ totalVotes);
-		connection.query({query:"SELECT Id FROM "+tableName+" WHERE "+ followingField +" = '"+followingVotesName+"'"},function(error,response){
+function initializeTotals(tableName,callbackFunction){
+	connection.query({query:'SELECT Count(Id) FROM ' + tableName}, function(error, response){
+		handleCallbackErrors(error, response);
+		totalVotes = response.records[0].get("expr0");
+		console.log("Votes: " + totalVotes);
+		connection.query({query:"SELECT Id, Vote__c, Count(Vote__c) FROM Survey_Response__c GROUP BY Vote__c"}, function(error,response){
+			handleCallbackErrors(error, response);
+			console.log(print(response));
+			exit();
 			followingVotes = response.records.length;
 			console.log('percentage: '+followingVotes/totalVotes);
 			callbackFunction();
@@ -64,45 +48,39 @@ function initializingTotals(tableName,callbackFunction){
 	});
 }
 
-function basicLittleQuery(){
-	var query = 'SELECT Id, Vote__c FROM Survey_Response__c';
-	connection.query({query:query}, queryCallback);
-}
-
 function subscribe(topicName){
-	console.log('Opening stream ' + topicName);
+	console.log('Opening stream to ' + topicName);
 	var stream = connection.stream({topic:topicName});
-	stream.on('connect', function( ){ console.log('connected' ); });
-	stream.on('error',   function(e){ console.log('error ' + e); });
+	stream.on('connect', function(){console.log('connected');});
+	stream.on('error',   function(e){console.log('error ' + e);});
 	stream.on('data',    handleData);
-	console.log('Returning stream ' + stream);
+	console.log('Opened stream ' + print(stream));
 	return stream;
 }
 
 function handleData(data){
-	console.log('got stream data ' + data.sobject.Vote__c);
+	console.log('Got stream data ' + data.sobject.Vote__c);
 	totalVotes++;
 	if(data.sobject.Vote__c == followingVotesName){
 		followingVotes++;
 	}
-	console.log('percentage: '+followingVotes/totalVotes);
+	console.log('percentage: ' + followingVotes/totalVotes);
+}
+
+function authenticationCallback(error, response){
+	//console.log('In authentication callback.');
+	handleCallbackErrors(error, response);
+	console.log('Authenticated and received token ' + connection.oauth.access_token);
+	main();
 }
 
 function main(){
-	console.log('Starting in main().');		
-
-	initializingTotals(followingTable,function() {
-		subscribe('AllVotes');
-	});
-
-	console.log('Done in main().');
+	console.log('In main.');		
+	initializeTotals(followingTable,function(){subscribe('AllVotes');});
 }
 
-
-console.log('Welcome to our node script!');
-console.log('Creating connection.');
-var nforce = require('nforce');
-var connection = nforce.createConnection(appCredentials);
+console.log('Connecting.');
+var connection = require('nforce').createConnection(appCredentials);
 console.log('Authenticating.');
 connection.authenticate(sfCredentials, authenticationCallback);
 
