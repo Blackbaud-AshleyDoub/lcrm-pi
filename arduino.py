ac874ddcb88124a74e377e804bca5313566c4d3f
Craig Pemberton <craig.pemberton@blackbaud.com>
2014-02-27 23:19:13 -0600

    jellyfish

--- arduino.py
+++ arduino.py
@@ -2,12 +2,53 @@
 
 import fileinput
 import pyfirmata
+import time
+import sys
 
-watchFor = 'Susan G. Komen'
 
-board = pyfirmata.Arduino('/dev/ttyACM0');
+def flashOnBoardLED(board):
+	led = board.get_pin('d:13:o')
+	while True:
+		print 'beep!'
+		led.write(1)
+		time.sleep(1)
+		led.write(0)
+		time.sleep(1)
 
-for line in fileinput.input():
-	percentage, cause = line.strip().split('\t')
-	if cause == watchFor:
-		print percentage
+def board():
+	print 'Initializing board...',
+	sys.stdout.flush()
+	board = pyfirmata.Arduino('/dev/ttyACM0')
+	print 'done'
+	sys.stdout.flush()
+	#jellyfish()
+	flashOnBoardLED(board)
+
+def jellyfish():
+	pins = []
+	for pin in range(2,12):
+		print 'Initializing pin', pin
+		pins.append(board.get_pin('d:' + str(pin).zfill(2) + ':o'))
+	state = True;
+	while True:
+		for pin in pins:
+			toggle(pin, state)
+			time.sleep(1)
+		state = not state
+	board.exit()
+
+def toggle(pin, state):
+	print 'setting pin to ' + str(state)
+	if state:
+		pin.write(1)
+	else:
+		pin.write(0)
+
+def readInput():
+	watchFor = 'Susan G. Komen'
+	for line in fileinput.input():
+		percentage, cause = line.strip().split('\t')
+		if cause == watchFor:
+			print percentage
+
+board()
